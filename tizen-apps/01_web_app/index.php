8f17445b4be79afc27bcfd62357d1799fbd18ce6
thegreeneyl <service@thegreeneyl.com>
2020-09-30 18:03:54 +0200

    Web-App Interface

--- tizen-apps/01_web_app/index.php
+++ tizen-apps/01_web_app/index.php
@@ -42,7 +42,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=0.48, user-scalable=no">
     <title>Monitor Steuerung</title>
-    <link href="style.css" rel="stylesheet">
+    <link href="style.css?q=734zr34fh" rel="stylesheet">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 	<link rel="icon" href="favicon.ico" type="image/x-icon">
   </head>
